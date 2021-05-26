0ca19ea02d766e4e22ea770c88f7ea3d4a7e6f77
Gjorgji Belkoski <gbelkoski@hotmail.com>
2021-05-26 16:30:01 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub btnClickThis_Click(sender As Object, e As EventArgs) Handles btnClickThis.Click
+
+        lblHelloWorld.Text = "Hello World!!!"
+    End Sub
+End Class
